213a9514ddf9ecca7f2ceb9c76200850d5c3918c
Gabriel T Marques <gabriel.teodosio@fastsolucoes.com.br>
2022-04-14 12:46:16 -0300

    added Reactotron to help with debugging

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
+import './config/Reactotron';
 import { routes } from './routes/routes';
 import { store, persistor } from './store';
 
@@ -26,18 +28,20 @@ const App = () => {
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-        <NavigationContainer>
-          <MainStack.Navigator initialRouteName='Login'>
-            {routes.map((route, idx) => (
-              <MainStack.Screen
-                key={`${new Date().getTime()}-screen-${idx}`}
-                name={route.name}
-                options={route.options}
-                component={route.component}
-              />
-            ))}
-          </MainStack.Navigator>
-        </NavigationContainer>
+        <SafeAreaProvider>
+          <NavigationContainer>
+            <MainStack.Navigator initialRouteName='Login'>
+              {routes.map((route, idx) => (
+                <MainStack.Screen
+                  key={`${new Date().getTime()}-screen-${idx}`}
+                  name={route.name}
+                  options={route.options}
+                  component={route.component}
+                />
+              ))}
+            </MainStack.Navigator>
+          </NavigationContainer>
+        </SafeAreaProvider>
       </PersistGate>
     </Provider>
   );
---
+++ src/config/Reactotron.js
@@ -0,0 +1,35 @@
+﻿import Reactotron, {
+  overlay,
+  networking,
+  asyncStorage,
+  openInEditor,
+  trackGlobalErrors,
+} from 'reactotron-react-native';
+import sagaPlugin from 'reactotron-redux-saga';
+import { reactotronRedux } from 'reactotron-redux';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const tron = Reactotron.configure({ name: 'CheckPlantApp', port: 9091 })
+  .use(overlay())
+  .use(sagaPlugin())
+  .use(networking())
+  .use(openInEditor())
+  .use(asyncStorage({}))
+  .use(reactotronRedux())
+  .use(trackGlobalErrors({}))
+  .useReactNative({
+    errors: true,
+    editor: true,
+    devTools: true,
+    storybook: true,
+    networking: true,
+    asyncStorage: true,
+  })
+  .setAsyncStorageHandler(AsyncStorage)
+  .connect();
+
+console.tron = tron;
+
+if (__DEV__) {
+  console.tron.clear();
+}
